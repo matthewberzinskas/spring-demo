0b47d2dbd3498200349c79caf84d6f83d78537f0
matthewberzinskas <pasomatt@gmail.com>
2022-08-01 11:54:00 -0700

    Initial project setup.

---
+++ src/test/java/com/mberz2/backend/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mberz2.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
